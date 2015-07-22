1a4de2596299391b8d6114aa2104143fd7106f14
yc <yc95227@gmail.com>
2015-07-22 15:41:15 +0800

    2->4

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def index(request):
     category_list = Category.objects.all()
     # 文章查询
     article_list = Article.objects.all()
-    paginator = Paginator(article_list, 2)     # 2为分页数
+    paginator = Paginator(article_list, 4)     # 2为分页数
     try:
         page = request.GET.get('page', 1)
         article_list = paginator.page(page)   # 返回当前页数据，保存在article_list中，page为第几个页面
